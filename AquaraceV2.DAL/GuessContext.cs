d3789d5041b46fcf05fc5f5795890a477c425fb7
MartinRuis <ruismartin98@gmail.com>
2018-11-29 13:07:28 +0100

    dev stuff

--- AquaraceV2.DAL/GuessContext.cs
+++ AquaraceV2.DAL/GuessContext.cs
@@ -31,11 +31,13 @@ namespace AquaraceV2.DAL
             ExecuteInsertProcedure("create_verstappen_placement", parameters);
         }
 
-        public List<int> Check_Guess_Existence(int player_id)
+        public List<int> Check_Guess_Existence(int player_id, int group_id, int race_id)
         {
             List<SqlParameter> parameters = new List<SqlParameter>();
             parameters.Add(new SqlParameter("@player_id", player_id));
-            string[] x = { "driver_id" };
+            parameters.Add(new SqlParameter("@group_id", group_id));
+            parameters.Add(new SqlParameter("@race_id", race_id));
+            string[] x = { "driver_id", };
             List<object> o = ExecuteSelectProcedure("check_guess_existence", parameters, 1, x);
             List<int> returnobject = new List<int>();
             foreach (object ob in o)
---
+++ AquaraceV2.Tests/GuessTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AquaraceV2.Tests
+{
+    [TestClass]
+    public class GuessTests
+    {
+        [TestMethod]
+        public void test_guess_check()
+        {
+            //Arange
+            bool expected = true;
+            List<int> memberIds = new List<int>();
+            memberIds.Add(1);
+            memberIds.Add(2);
+            memberIds.Add(3);
+            memberIds.Add(4);
+            int self_id = 1;
+            int[] driverIds = { 2,5,4,9,15};
+
+            //Act
+
+            //Assert
+        }
+    }
+}
